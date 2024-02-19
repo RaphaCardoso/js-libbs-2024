e951f049ce9d1eef6dd827b2c187510082ed2bf1
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-02-19 16:06:19 -0300

    Update script.js

--- aula-04/script.js
+++ aula-04/script.js
@@ -4,12 +4,55 @@ const fname = document.getElementById("fname");
 
 // Para "ouvir" o click do botão, usamos o addEventListener
 
+const lname = document.getElementById("lname");
+
+const Mail = document.getElementById("Mail");
+
+const fone = document.getElementById("fone");
+
+const end = document.getElementById("end");
+
 const btnSubmit = document.getElementById("btn");
+
+//criar uma lista vazia
+const data = [];
+
 
 btnSubmit.addEventListener('click', function (event) {
 
     // Não deixa o formulário se comportar como padrão, ou seja,
     // enviar os dados para próxima página / reuisição
     event.preventDefault();
-    console.log("cliquei!!!!");
-})
+
+
+    // criando objeto
+    const pessoa = {
+        nome: fname.value,
+        endereco: end.value,
+        email: Mail.value,
+        fone: fone.value,
+
+
+        favoritos: [
+            {
+                título: "A nova casa",
+                autor: "Juca Bala",
+                ano: 2025,
+                genero: "Aventura"
+            }
+        ]
+    }
+
+    // adicionar pessoa na lista
+
+    data.push(pessoa);
+    console.log(pessoa.favoritos);
+    console.log(data);
+
+});
+
+
+
+
+
+
