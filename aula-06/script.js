6fd6da999789afeea64889e1ad84927dc303c29f
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-03-12 08:56:59 -0300

    Create script.js

---
+++ aula-06/script.js
@@ -0,0 +1,84 @@
+// Pegando o id do html
+/*const main = document.getElementById('principal');
+
+// Criando objetos
+
+const myItems = [
+
+    {
+        color: 'blue',
+        text: 'Primeiro item'
+    },
+    {
+        color: 'red',
+        text: 'Segundo item'
+    },
+    {
+        color: 'yellow',
+        text: 'Terceiro item'
+    },
+    {
+        color: 'brown',
+        text: 'Quarto item'
+    },
+    {
+        color: 'green',
+        text: 'Quinto item'
+    },
+
+];
+
+
+// Para ler o objeto e dar comandos para aparecer no browser
+
+myItems.forEach(item => {
+    const meuH1 = document.createElement('h1');
+    meuH1.innerText = item.text;
+    meuH1.style.color = item.color;
+    main.appendChild(meuH1);
+
+}); */
+
+
+const card = document.getElementById('card');
+const cardImage = document.getElementById('cardImage');
+const image = document.getElementById('image');
+const nome = document.getElementById('nome')
+const linha = document.getElementById('linha');
+const diaIcone = document.getElementById('diaIcone');
+const dia = document.getElementById('dia');
+const icone = document.getElementById('icone');
+
+
+/*
+
+const myCard = [
+    {
+        cardImage: [image = ['londres', 'Madrid', 'Paris', 'rio', 'Salvador', 'Veneza'], nome = ['LONDON 60&deg; F', 'MADRID 50&deg; F', 'PARIS 40&deg; F', 'RIO DE JANEIRO 80&deg; F', 'SALVADOR 90&deg; F', 'VENEZA 50&deg; F']],
+        linha: [diaIcone = [dia = ['SEG', 'TER', 'QUA'], icone = ['Sol', 'Chuva', 'nuvens']]]
+
+    },
+    {
+        cardImage: [image = 'Madrid', nome = 'MADRID 50&deg; F'],
+        linha: [diaIcone = [dia = ['SEG', 'TER', 'QUA'], icone = ['SEG', 'TER', 'QUA']]]
+    }
+]
+
+myCard.forEach(card => {
+    const weather = document.createElement('div.w3-card-4');
+
+
+})*/
+
+
+/*const main = document.getElementById('card');
+
+const cardData = [
+    { city: 'Londres', temperature: '15°C', forecast: ['Seg', 'Ter', 'Qua'] },
+    { city: 'Paris', temperature: '18°C', forecast: ['Qua', 'Qui', 'Sex'] },
+
+
+];*/
+
+
+
